381c604b4c66fc5301aa051f55ee372a63a0baf0
Nick <Echophase44@github.com>
2022-10-09 06:49:29 -0400

    Organized Open/Close functions, started building out Book class and added library section

--- script.js
+++ script.js
@@ -3,11 +3,32 @@ const popup = document.querySelector("#popup");
 const formClose = document.querySelector(".formClose");
 
 addBook.addEventListener("click", () => {
-  popup.style.display = "block";
-  popup.classList.add("addBlur");
+  openPopup();
 });
 
 formClose.addEventListener("click", () => {
+  closePopup();
+});
+
+function openPopup() {
+  popup.style.display = "block";
+  popup.classList.add("addBlur");
+}
+
+function closePopup() {
   popup.style.display = "none";
   popup.classList.remove("addBlur");
-});
+}
+
+// ######################### Constructor #########################
+
+let myLibrary = [];
+
+class Book {
+  constructor(title, author, pages, read) {
+    this._title = title;
+    this._author = author;
+    this._pages = pages;
+    this._read = read;
+  }
+}
